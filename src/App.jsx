85b958fdd38db140f2530010b2273e4a22d25606
nimra-iqbal512 <nimraiqbal512@gmail.com>
2025-05-11 06:55:05 +0500

    Fetch and Display Images

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { storage } from "./config";
-import {ref, uploadBytes} from "firebase/storage";
+import {getDownloadURL, listAll, ref, uploadBytes} from "firebase/storage";
 import {v4} from "uuid";
 
 const App = () => {
   const [img, setImg] = useState('')
+  const [imgUrl, setImgUrl] = useState([])
 
   const handleClick = ()=>{
     const imgRef = ref(storage, `UniLogoFiles/${v4()}`)
     uploadBytes(imgRef, img)
   }
 
+  useEffect(() => {
+    listAll(ref(storage, "UniLogoFiles")).then(imgs => {
+      console.log(imgs);
+      imgs.items.forEach(val => {
+        getDownloadURL(val).then(url => {
+          setImgUrl(data=>[...data, url])
+        })
+      })
+    })
+  }, []);
+
+  
+
   return <div>
       <input type="file" onChange={(e) => setImg(e.target.files[0])} />
       <button onClick={handleClick}>Upload</button>
+      <br />
+      {
+        imgUrl.map(dataVal=><div>
+          <img src={dataVal} height="200px" width="200px" />
+          <br />
+        </div>)
+      }
     </div>;
 };
 export default App;
